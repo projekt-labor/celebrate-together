8fd6cb66b6753c5d5c012e28c55d6717bdab543a
Martin Kondor <martinkondor@gmail.com>
2022-10-03 15:40:35 +0200

    init

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+
+// Global variables
+const APP = express();
+const PORT = 8080;
+const BASE_TITLE = "Celebrate Together";
+let counter = 0;
+
+
+// Settings
+APP.set("view engine", "pug");
+
+
+// Paths
+APP.get("/minus", (req, res) => {
+
+    res.render('index', {
+        title: BASE_TITLE + " - Minus",
+        message: --counter
+    });
+
+});
+
+APP.get('/plus', (req, res) => {
+
+    res.render('index', {
+        title: BASE_TITLE + " - Plus",
+        message: ++counter
+    });
+
+});
+
+
+// Starting up
+APP.listen(PORT, () => {
+    console.log(`Celebrate Together is up on http://127.0.0.1:${PORT}/`);
+});
