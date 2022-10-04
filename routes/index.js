a078a4749b9f47853baa3accc0d17040279250fb
Martin Kondor <martinkondor@gmail.com>
2022-10-04 16:27:41 +0200

    Regisztráció és bejelentkezés ✅

--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,89 @@ const INDEX_ROUTE = express.Router();
 const DB = require("../src/database");  
 
 const BASE_TITLE = "Celebrate Together";
+const REGISTER_NOK = "Nem megfelelő regisztrációs adatok";
+const LOGIN_NOK = "Nem megfelelő bejelentkezési adatok";
+const ERROR_MSG = "Probléma lépett fel a kérés teljesítése során";
 
 
-INDEX_ROUTE.get("/", (req, res) => {
-    const messages = req.consumeFlash('info');
+INDEX_ROUTE.get("/", async (req, res) => {
     res.render("index", {
         title: BASE_TITLE,
-        messages: messages
+        messages: await req.consumeFlash('info')
     });
 });
 
-/*
-const myPlaintextPassword = "kecske";
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
-            console.log(hash);
-        });
+INDEX_ROUTE.get("/register", async (req, res) => {
+    res.render("register", {
+        title: BASE_TITLE,
+        messages: await req.consumeFlash('info')
     });
-*/
+});
+
+INDEX_ROUTE.post("/register", async (req, res) => {
+    req.checkBody("email", "The email field cannot be empty.")
+        .isEmail()
+        .isLength({ min: 1 });
+    req.checkBody("password", "The password field cannot be empty.")
+        .isLength({ min: 1 });
+    req.checkBody("password_again", "The password again field cannot be empty.")
+        .isLength({ min: 1 });
+    req.checkBody("birthday", "The birthday field cannot be empty.")
+        .isLength({ min: 4 });
+
+    const errors = req.validationErrors();
+    const email = req.body.email;
+    const password = req.body.password;
+    const passwordAgain = req.body.password_again;
+    const birthday = req.body.birthday;
+
+    if (errors) {
+        console.log("\nHiba a bemenetekkel!\n");
+        req.flash('info', REGISTER_NOK);
+        return res.redirect("/");
+    }
+
+    if (password != passwordAgain) {
+        req.flash('info', "A megadott jelszavak nem egyeznek!");
+        return res.redirect("/register");
+    }
+
+    // 2. Felhasználó létrehozása
+    const createAndSaveUser = (callback) => {
+        return bcrypt.genSalt(10, (err, salt) => {
+            return bcrypt.hash(password, salt, function(err, hash) {
+                return DB.query("INSERT INTO users (email, password) VALUES (?, ?)", [email, hash], callback);
+            });
+        });
+    }
+
+    // 1. Keressük hogy az email foglalt-e?
+    const isTheEmailReserved = (error, databaseResults) => {
+        if (error) {
+            req.flash('info', ERROR_MSG);
+            return res.redirect("/");
+        }
+        if (databaseResults.length != 0) {
+            console.log("\nVan már ilyen email-el felhasználó!\n");
+            req.flash('info', REGISTER_NOK);
+            return res.redirect("/register");
+        }
+        return createAndSaveUser((error, result) => {
+            if (error) {
+                req.flash('info', ERROR_MSG);
+                return res.redirect("/");
+            }
+            return res.redirect("/");
+        });
+    };
+
+    return DB.query('SELECT u.email FROM users u WHERE u.email=?', [email], isTheEmailReserved);
+});
 
 INDEX_ROUTE.get("/login", async (req, res) => {
-    const messages = await req.consumeFlash('info');
-
     res.render("login", {
         title: BASE_TITLE,
-        messages: messages
+        messages: await req.consumeFlash('info')
     });
 });
 
@@ -43,25 +101,25 @@ INDEX_ROUTE.post("/login", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
         
-    if (!email || !password || errors) {
-        req.flash('info', "Nem megfelelő bejelentkezési adatok!");
+    if (errors) {
+        req.flash('info', LOGIN_NOK);
         return res.redirect("/login");
     }
 
     const databaseResCallback = (error, databaseResults) => {
-            if (error || !databaseResults || !databaseResults[0]) {
-                req.flash('info', "Nem megfelelő bejelentkezési adatok!");
+            if (error || !databaseResults || databaseResults.length == 0) {
+                req.flash('info', LOGIN_NOK);
                 return res.redirect("/login");
             }
 
             return bcrypt.compare(password, databaseResults[0].password, (error, isMatch) => {
                 if (error) {
                     console.error(error);
-                    req.flash('info', "Hiba a szerverrel!");
+                    req.flash('info', ERROR_MSG);
                     return res.redirect("/login");
                 }
                 if (!isMatch) {
-                    req.flash('info', "Nem megfelelő bejelentkezési adatok!");
+                    req.flash('info', LOGIN_NOK);
                     return res.redirect("/login");
                 }
                 return res.redirect("/");
