d21235797a2dd29c46ff0a071adfb61be1861264
Martin Kondor <martinkondor@gmail.com>
2022-11-22 12:14:16 +0100

    ...

--- routes/event.js
+++ routes/event.js
@@ -245,7 +245,7 @@ EVENT_ROUTE.get("/:id/:name", onlyLogined, async (req, res) => {
 
         console.log(results[0]);
 
-        return await DB.query("SELECT * FROM user_event_switch ue LEFT JOIN user u ON(u.id=ue.user_id) WHERE ue.event_id=?;"
+        return DB.query("SELECT * FROM user_event_switch ue LEFT JOIN user u ON(u.id=ue.user_id) WHERE ue.event_id=?;"
         [req.params.id],
         async (errors, attendants) => {
             if (errors) {
